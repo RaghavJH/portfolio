7dc8ea9ae06fc182f22a58d0d302d3be25a7906a
Raghav Jhavar <raghav@jhavar.net>
2020-05-30 20:02:14 -0700

    Updates

---
+++ precache-manifest.e54696787db8afa7c4120d13751e0e26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06441d5834771ab3c12ac037e06628bb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2b052ab889d7dccd6440",
+    "url": "/portfolio/static/css/main.bf900766.chunk.css"
+  },
+  {
+    "revision": "3fd00a124dcb1e4924aa",
+    "url": "/portfolio/static/js/2.d755492c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d755492c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b052ab889d7dccd6440",
+    "url": "/portfolio/static/js/main.605f8dde.chunk.js"
+  },
+  {
+    "revision": "d035cac9cfccd6da8be6",
+    "url": "/portfolio/static/js/runtime-main.a982299b.js"
+  },
+  {
+    "revision": "0cdf769403fff4e7ea02433be8940c2a",
+    "url": "/portfolio/static/media/0.0cdf7694.jpg"
+  },
+  {
+    "revision": "418e99f239aa884e58a6196ec143c8c6",
+    "url": "/portfolio/static/media/resume.418e99f2.pdf"
+  }
+]);
